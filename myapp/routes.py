7561a91a6d317c1a630cc1b3cb95622e9a0261bf
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2019-12-18 11:16:10 +0100

    Por olvido

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,9 +1,43 @@
 from myapp import app
 from flask import render_template
+import csv
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    '''
+    leer el fichero sales10.csv y transformarlo en un diccionario
+    '''
+    fSales = open('../data/sales10.csv','r')
+
+    csvreader = csv.reader(fSales, delimiter=',')
+    registros = []
+    for linea in csvreader:
+        registros.append(linea)
+
+    cabecera = registros[0]
+
+    ventas = []
+    for datos in registros[1:]:
+        d = {}
+        for ix, nombre_campo in enumerate(cabecera): 
+            d[nombre_campo] = datos[ix]
+        ventas.append(d)
+
+
+    '''
+    procesarlo para obtener los totales
+    '''
+    resultado = [] #lista de diccionarios
+    '''
+    {'region': 'Australia and Oceania', 'ingresos_totales': 3292856.72, 'beneficios_totales': 1236498.14}
+    {'region': ...}
+    '''
+
+    '''
+    enviarlo a index.html
+    '''
+
+    return render_template('index.html', registros=resultado)
 
 @app.route("/detail")
 def detail():
