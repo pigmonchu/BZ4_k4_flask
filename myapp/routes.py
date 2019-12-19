1a7a5975f43be2b535ef4f540976a0a729d3a46a
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2019-12-19 22:39:28 +0100

    A ver si ahora. parametros por ruta y css

--- myapp/routes.py
+++ myapp/routes.py
@@ -2,32 +2,30 @@ from myapp import app
 from flask import render_template
 import csv
 
+'''
+leer el fichero sales10.csv y transformarlo en un diccionario
+'''
 
+fSales = open('./data/sales10.csv','r')
+
+csvreader = csv.reader(fSales, delimiter=',')
+registros = []
+for linea in csvreader:
+    registros.append(linea)
+    print(linea)
+
+cabecera = registros[0]
+
+ventas = []
+for datos in registros[1:]:
+    d = {}
+
+    for ix, nombre_campo in enumerate(cabecera): 
+        d[nombre_campo] = datos[ix]
+    ventas.append(d)
 
 @app.route("/")
 def index():
-    '''
-    leer el fichero sales10.csv y transformarlo en un diccionario
-    '''
-    
-    fSales = open('./data/sales10.csv','r')
-
-    csvreader = csv.reader(fSales, delimiter=',')
-    registros = []
-    for linea in csvreader:
-        registros.append(linea)
-        print(linea)
-
-    cabecera = registros[0]
-
-    ventas = []
-    for datos in registros[1:]:
-        d = {}
-        for ix, nombre_campo in enumerate(cabecera): 
-            d[nombre_campo] = datos[ix]
-        ventas.append(d)
-
-
     '''
     procesarlo para obtener los totales
     '''
@@ -53,6 +51,19 @@ def index():
     '''
     return render_template('index.html', registros=resultado)
 
-@app.route("/detail")
-def detail():
-    return render_template('detail.html')
+@app.route("/detail/<region_name>")
+def detail(region_name):
+
+    datos = {}
+    for linea in ventas:
+        print(linea['region'])
+        if linea['region'] == region_name:
+            if linea['pais'] in datos:
+                regAct = datos[linea['pais']]
+                regAct['ingresos_totales'] += float(linea['ingresos_totales'])
+                regAct['beneficios_totales'] += float(linea['beneficio'])
+            else:
+                datos[linea['pais']] = {'ingresos_totales': float(linea['ingresos_totales']), 'beneficios_totales': float(linea['beneficio'])}
+
+    print(datos)
+    return render_template('detail.html', region=region_name, registros=datos)
